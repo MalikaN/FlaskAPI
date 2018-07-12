55c6736f99f10647bf55bf3e6dedf69210d149df
Malika <malika@Tharakas-MacBook-Air.local>
2018-07-13 00:19:45 +0530

    database changes

--- pihitakapi/config.py
+++ pihitakapi/config.py
@@ -2,7 +2,7 @@ class BaseConfig(object):
     DEBUG = True
     MYSQL_DATABASE_USER = "root"
     MYSQL_DATABASE_PASSWORD = ""
-    MYSQL_DATABASE_DB = "myproject"
+    MYSQL_DATABASE_DB = "Pihitak"
     MYSQL_DATABASE_HOST = "localhost"
     # used for encryption and session management
     JWT_SECRET_KEY = 'mysecretkey'
