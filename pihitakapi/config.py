34928ad432a6b2983a6c822e077d9bbbc7c3d2fb
Malika <malika@Tharakas-MacBook-Air.local>
2018-07-13 10:58:47 +0530

    config file changes

--- pihitakapi/config.py
+++ pihitakapi/config.py
@@ -1,7 +1,7 @@
 class BaseConfig(object):  
     DEBUG = True
-    MYSQL_DATABASE_USER = "root"
-    MYSQL_DATABASE_PASSWORD = ""
+    MYSQL_DATABASE_USER = "admin"
+    MYSQL_DATABASE_PASSWORD = "pwd123"
     MYSQL_DATABASE_DB = "Pihitak"
     MYSQL_DATABASE_HOST = "localhost"
     # used for encryption and session management
