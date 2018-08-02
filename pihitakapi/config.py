004cdeab5cf2e471452baee99d93475a099e986c
Malika <malika@Tharakas-MacBook-Air.local>
2018-08-02 10:08:45 +0530

    configuration to DB changed in config.py

--- pihitakapi/config.py
+++ pihitakapi/config.py
@@ -11,7 +11,7 @@ class BaseConfig(object):
     #FOR EC2
     MYSQL_DATABASE_USER = "admin"
     MYSQL_DATABASE_PASSWORD = "pwd123"
-    MYSQL_DATABASE_DB = "pihitak"
+    MYSQL_DATABASE_DB = "Pihitak"
     MYSQL_DATABASE_HOST = "localhost"
     # used for encryption and session management
     JWT_SECRET_KEY = 'mysecretkey'
