2a82cce33e7fd95eb6d9797c28f576bd944440e7
Malika <malika@Tharakas-MacBook-Air.local>
2018-08-12 23:00:09 +0530

    getPost changes

--- pihitakapi/config.py
+++ pihitakapi/config.py
@@ -1,19 +1,19 @@
 class BaseConfig(object):  
     DEBUG = True
     #FOR LOCAL
-    MYSQL_DATABASE_USER = "root"
-    MYSQL_DATABASE_PASSWORD = ""
-    MYSQL_DATABASE_DB = "myproject"
-    MYSQL_DATABASE_HOST = "localhost"
-    # used for encryption and session management
-    JWT_SECRET_KEY = 'mysecretkey'
+    # MYSQL_DATABASE_USER = "root"
+    # MYSQL_DATABASE_PASSWORD = ""
+    # MYSQL_DATABASE_DB = "myproject"
+    # MYSQL_DATABASE_HOST = "localhost"
+    # # used for encryption and session management
+    # JWT_SECRET_KEY = 'mysecretkey'
 
 
     
     #FOR EC2
-    # MYSQL_DATABASE_USER = "admin"
-    # MYSQL_DATABASE_PASSWORD = "pwd123"
-    # MYSQL_DATABASE_DB = "Pihitak"
-    # MYSQL_DATABASE_HOST = "localhost"
-    # # used for encryption and session management
-    # JWT_SECRET_KEY = 'mysecretkey'
+    MYSQL_DATABASE_USER = "admin"
+    MYSQL_DATABASE_PASSWORD = "pwd123"
+    MYSQL_DATABASE_DB = "Pihitak"
+    MYSQL_DATABASE_HOST = "localhost"
+    # used for encryption and session management
+    JWT_SECRET_KEY = 'mysecretkey'
